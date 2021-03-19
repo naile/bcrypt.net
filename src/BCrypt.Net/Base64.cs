696c4df4a84a5bc6ec450c4cd5cf8d9908b48b8f
Andreas Caravella <andreas.caravella@gmail.com>
2021-03-19 22:41:43 +0100

    refactor, extract SaltGenerator and Base64 to own classes

---
+++ src/BCrypt.Net/Base64.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BCrypt.Net
+{
+    internal static class Base64
+    {
+#if HAS_SPAN
+        /// <summary>
+        ///  Encode a byte array using BCrypt's slightly-modified base64 encoding scheme. Note that this
+        ///  is *not* compatible with the standard MIME-base64 encoding.
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when one or more arguments have unsupported or
+        ///                                     illegal values.</exception>
+        /// <param name="byteArray">The byte array to encode.</param>
+        /// <param name="length">   The number of bytes to encode.</param>
+        /// <param name="outBuffer">The output buffer</param>
+        /// <param name="pos"></param>
+        /// <param name="charsWritten"></param>
+        /// <returns>Base64-encoded string.</returns>
+        public static void EncodeBase64(Span<byte> byteArray, int length, Span<char> outBuffer, int pos = 0)
+        {
+            if (length <= 0 || length > byteArray.Length)
+            {
+                throw new ArgumentException("Invalid length", nameof(length));
+            }
+
+            Span<char> encoded = outBuffer;
+
+            int off = 0;
+            while (off < length)
+            {
+                int c1 = byteArray[off++] & 0xff;
+                encoded[pos++] = Base64Code[(c1 >> 2) & 0x3f];
+                c1 = (c1 & 0x03) << 4;
+                if (off >= length)
+                {
+                    encoded[pos++] = Base64Code[c1 & 0x3f];
+                    break;
+                }
+
+                int c2 = byteArray[off++] & 0xff;
+                c1 |= (c2 >> 4) & 0x0f;
+                encoded[pos++] = Base64Code[c1 & 0x3f];
+                c1 = (c2 & 0x0f) << 2;
+                if (off >= length)
+                {
+                    encoded[pos++] = Base64Code[c1 & 0x3f];
+                    break;
+                }
+
+                c2 = byteArray[off++] & 0xff;
+                c1 |= (c2 >> 6) & 0x03;
+                encoded[pos++] = Base64Code[c1 & 0x3f];
+                encoded[pos++] = Base64Code[c2 & 0x3f];
+            }
+        }
+#endif
+
+        /// <summary>
+        ///  Encode a byte array using BCrypt's slightly-modified base64 encoding scheme. Note that this
+        ///  is *not* compatible with the standard MIME-base64 encoding.
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when one or more arguments have unsupported or
+        ///                                     illegal values.</exception>
+        /// <param name="byteArray">The byte array to encode.</param>
+        /// <param name="length">   The number of bytes to encode.</param>
+        /// <returns>Base64-encoded string.</returns>
+#if HAS_SPAN
+        public static char[] EncodeBase64(Span<byte> byteArray, int length)
+#else
+        public static char[] EncodeBase64(byte[] byteArray, int length)
+#endif
+        {
+            if (length <= 0 || length > byteArray.Length)
+            {
+                throw new ArgumentException("Invalid length", nameof(length));
+            }
+
+            int encodedSize = (int)Math.Ceiling((length * 4D) / 3);
+            char[] encoded = new char[encodedSize];
+
+            int pos = 0;
+            int off = 0;
+            while (off < length)
+            {
+                int c1 = byteArray[off++] & 0xff;
+                encoded[pos++] = Base64Code[(c1 >> 2) & 0x3f];
+                c1 = (c1 & 0x03) << 4;
+                if (off >= length)
+                {
+                    encoded[pos++] = Base64Code[c1 & 0x3f];
+                    break;
+                }
+
+                int c2 = byteArray[off++] & 0xff;
+                c1 |= (c2 >> 4) & 0x0f;
+                encoded[pos++] = Base64Code[c1 & 0x3f];
+                c1 = (c2 & 0x0f) << 2;
+                if (off >= length)
+                {
+                    encoded[pos++] = Base64Code[c1 & 0x3f];
+                    break;
+                }
+
+                c2 = byteArray[off++] & 0xff;
+                c1 |= (c2 >> 6) & 0x03;
+                encoded[pos++] = Base64Code[c1 & 0x3f];
+                encoded[pos++] = Base64Code[c2 & 0x3f];
+            }
+
+            return encoded;
+        }
+
+        /// <summary>
+        ///  Decode a string encoded using BCrypt's base64 scheme to a byte array.
+        ///  Note that this is *not* compatible with the standard MIME-base64 encoding.
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when one or more arguments have unsupported or
+        ///                                     illegal values.</exception>
+        /// <param name="encodedString">The string to decode.</param>
+        /// <param name="maximumBytes"> The maximum bytes to decode.</param>
+        /// <returns>The decoded byte array.</returns>
+        public static byte[] DecodeBase64(string encodedString, int maximumBytes)
+        {
+            int sourceLength = encodedString.Length;
+            int outputLength = 0;
+
+            if (maximumBytes <= 0)
+            {
+                throw new ArgumentException("Invalid maximum bytes value", nameof(maximumBytes));
+            }
+
+            byte[] result = new byte[maximumBytes];
+
+            int position = 0;
+            while (position < sourceLength - 1 && outputLength < maximumBytes)
+            {
+                int c1 = Char64(encodedString[position++]);
+                int c2 = Char64(encodedString[position++]);
+                if (c1 == -1 || c2 == -1)
+                {
+                    break;
+                }
+
+                result[outputLength] = (byte)((c1 << 2) | ((c2 & 0x30) >> 4));
+                if (++outputLength >= maximumBytes || position >= sourceLength)
+                {
+                    break;
+                }
+
+                int c3 = Char64(encodedString[position++]);
+                if (c3 == -1)
+                {
+                    break;
+                }
+
+                result[outputLength] = (byte)(((c2 & 0x0f) << 4) | ((c3 & 0x3c) >> 2));
+                if (++outputLength >= maximumBytes || position >= sourceLength)
+                {
+                    break;
+                }
+
+                int c4 = Char64(encodedString[position++]);
+                result[outputLength] = (byte)(((c3 & 0x03) << 6) | c4);
+
+                ++outputLength;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        ///  Look up the 3 bits base64-encoded by the specified character, range-checking against
+        ///  conversion table.
+        /// </summary>
+        /// <param name="character">The base64-encoded value.</param>
+        /// <returns>The decoded value of x.</returns>
+        /// 
+        private static int Char64(char character)
+        {
+            return character < 0 || character > Index64.Length ? -1 : Index64[character];
+        }
+
+        // Table for Base64 encoding
+        private static readonly char[] Base64Code = {
+            '.', '/', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
+            '6', '7', '8', '9'
+        };
+
+        // Table for Base64 decoding
+        private static readonly int[] Index64 = {
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, 0, 1, 54, 55,
+            56, 57, 58, 59, 60, 61, 62, 63, -1, -1,
+            -1, -1, -1, -1, -1, 2, 3, 4, 5, 6,
+            7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+            -1, -1, -1, -1, -1, -1, 28, 29, 30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+            41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+            51, 52, 53, -1, -1, -1, -1, -1
+        };
+    }
+}
---
+++ src/BCrypt.Net/SaltGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BCrypt.Net
+{
+    internal static class SaltGenerator
+    {
+        /// <summary>
+        /// RandomNumberGenerator.Create calls RandomNumberGenerator.Create("System.Security.Cryptography.RandomNumberGenerator"), which will create an instance of RNGCryptoServiceProvider.
+        /// https://msdn.microsoft.com/en-us/library/42ks8fz1
+        /// </summary>
+        private static readonly RandomNumberGenerator RngCsp = RandomNumberGenerator.Create(); // secure PRNG
+
+        /// <summary>
+        ///  Generate a salt for use with the <see cref="BCrypt.HashPassword(string, string)"/> method.
+        /// </summary>
+        /// <param name="workFactor">The log2 of the number of rounds of hashing to apply - the work
+        ///                          factor therefore increases as 2**workFactor.</param>
+        /// <param name="bcryptMinorRevision"></param>
+        /// <exception cref="ArgumentOutOfRangeException">Work factor must be between 4 and 31</exception>
+        /// <returns>A base64 encoded salt value.</returns>
+        /// <exception cref="ArgumentException">BCrypt Revision should be a, b, x or y</exception>
+        public static string GenerateSalt(int workFactor, char bcryptMinorRevision = BCrypt.DefaultHashVersion)
+        {
+            if (workFactor < BCrypt.MinRounds || workFactor > BCrypt.MaxRounds)
+            {
+                throw new ArgumentOutOfRangeException(nameof(workFactor), workFactor, $"The work factor must be between {BCrypt.MinRounds} and {BCrypt.MaxRounds} (inclusive)");
+            }
+
+            if (bcryptMinorRevision != 'a' && bcryptMinorRevision != 'b' && bcryptMinorRevision != 'x' && bcryptMinorRevision != 'y')
+            {
+                throw new ArgumentException("BCrypt Revision should be a, b, x or y", nameof(bcryptMinorRevision));
+            }
+
+#if HAS_SPAN_RNG
+            Span<char> saltBuffer = stackalloc char[29];
+            var pos = 0;
+            saltBuffer[pos++] = '$';
+            saltBuffer[pos++] = '2';
+            saltBuffer[pos++] = bcryptMinorRevision;
+            saltBuffer[pos++] = '$';
+            saltBuffer[pos++] = (char)((workFactor / 10) + '0');
+            saltBuffer[pos++] = (char)((workFactor % 10) + '0');
+            saltBuffer[pos++] = '$';
+            WriteBase64Salt(saltBuffer, pos);
+            return new string(saltBuffer);
+#else
+            byte[] saltBytes = new byte[BCrypt.BCryptSaltLen];
+            RngCsp.GetBytes(saltBytes);
+            var result = new StringBuilder(29);
+            result.Append("$2").Append(bcryptMinorRevision).Append('$').Append(workFactor.ToString("D2")).Append('$');
+            result.Append(Base64.EncodeBase64(saltBytes, saltBytes.Length));
+
+            return result.ToString();
+#endif
+        }
+
+#if HAS_SPAN_RNG
+        private static void WriteBase64Salt(Span<char> outBuffer, int pos)
+        {
+            Span<byte> saltBytes = stackalloc byte[BCrypt.BCryptSaltLen];
+            RandomNumberGenerator.Fill(saltBytes);
+            Base64.EncodeBase64(saltBytes, saltBytes.Length, outBuffer, pos);
+        }
+#endif
+    }
+}
